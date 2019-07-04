b525ecc2f5a750c8a0210b60586f153a56cfebfa
tuxingyue <dwtxy@qq.com>
2019-07-04 22:31:22 +0800

    use c language

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
+
+#if 0
 #include<iostream>
 using namespace std;
-
 int main()
 {
 char a;
@@ -11,4 +12,14 @@ cin>>a>>b>>c;
 cout<<a<<b<<c<<endl;
 
 return 0;
+}
+
+#endif
+
+#include "stdio.h"
+
+int main()
+{
+	printf("hello world");
+	return 0;
 }
